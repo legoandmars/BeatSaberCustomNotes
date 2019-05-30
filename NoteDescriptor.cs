d2d7622a20d749b524ec14fcda1be905b1941687
Kyle1413 <kylet1412@gmail.com>
2019-05-29 22:38:43 -0400

    Cleanup, setup for Unity proj

---
+++ NoteDescriptor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace CustomNotes
+{
+    public class NoteDescriptor : MonoBehaviour
+    {
+        public string NoteName = "Note";
+        public string AuthorName = "Author";
+
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -15,72 +15,82 @@ namespace CustomNotes
     {
         internal static Ref<PluginConfig> config;
         internal static IConfigProvider configProvider;
-
-        public void Init(IPALogger logger, [Config.Prefer("json")] IConfigProvider cfgProvider) {
+        internal static Material noteMaterial = null;
+        internal static ColorManager colorManager;
+        public void Init(IPALogger logger)
+        {
             Logger.log = logger;
-            configProvider = cfgProvider;
-
-            config = cfgProvider.MakeLink<PluginConfig>((p, v) => {
-                if (v.Value == null || v.Value.RegenerateConfig)
-                    p.Store(v.Value = new PluginConfig() { RegenerateConfig = false });
-                config = v;
-            });
         }
+
         private AssetBundle assetBundle;
 
-        private GameObject cubeMesh;
-        private GameObject cubeMeshRed;
+        private NoteDescriptor selectedNoteDescriptor;
+        private GameObject noteLeft;
+        private GameObject noteRight;
 
         private List<string> customNotePaths;
         private string selectedNote;
 
-        public void OnApplicationStart() {
+        public void OnApplicationStart()
+        {
             Console.WriteLine("Starting CustomNotes!");
             loadCustomNotes();
         }
 
-        private bool IsValidPath(string path, bool allowRelativePaths = false) {
+        private bool IsValidPath(string path, bool allowRelativePaths = false)
+        {
             bool isValid = true;
 
-            try {
+            try
+            {
                 string fullPath = Path.GetFullPath(path);
 
-                if (allowRelativePaths) {
+                if (allowRelativePaths)
+                {
                     isValid = Path.IsPathRooted(path);
-                } else {
+                }
+                else
+                {
                     string root = Path.GetPathRoot(path);
                     isValid = string.IsNullOrEmpty(root.Trim(new char[] { '\\', '/' })) == false;
                 }
-            } catch (Exception ex) {
+            }
+            catch (Exception ex)
+            {
                 isValid = false;
             }
 
             return isValid;
         }
 
-        public void loadCustomNotes() {
+        public void loadCustomNotes()
+        {
             System.IO.Directory.CreateDirectory(Path.Combine(Application.dataPath, "../CustomNotes/"));
             customNotePaths = (Directory.GetFiles(Path.Combine(Application.dataPath, "../CustomNotes/"),
-                "*.note", SearchOption.AllDirectories).ToList());
+                "*.note", SearchOption.TopDirectoryOnly).ToList());
             Console.WriteLine("Found " + customNotePaths.Count + " note(s)");
             customNotePaths.Insert(0, "DefaultNotes");
-            customNotePaths.Insert(0, "CustomNotes.minecraft.note");
             LoadNoteAsset(customNotePaths[0]);
         }
 
-        public void OnApplicationQuit() {
+        public void OnApplicationQuit()
+        {
             Logger.log.Debug("OnApplicationQuit");
         }
 
-        public void OnFixedUpdate() {
+        public void OnFixedUpdate()
+        {
 
         }
 
-        public void OnUpdate() {
-            if (Input.GetKeyDown(KeyCode.N)) {
+        public void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.N))
+            {
                 if (customNotePaths.Count == 1) return;
                 var oldIndex = customNotePaths.IndexOf(selectedNote);
-                if (oldIndex >= customNotePaths.Count - 1) {
+                if (oldIndex >= customNotePaths.Count - 1)
+                {
                     oldIndex = -1;
                 }
 
@@ -89,99 +99,149 @@ namespace CustomNotes
             }
         }
 
-        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene) {
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+        {
 
         }
 
-        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode) {
-            if (scene.name == "GameCore") {
+        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+            if (scene.name == "GameCore")
+            {
                 Console.WriteLine("Custom Notes - Loading Scene");
                 var spawnController = Resources.FindObjectsOfTypeAll<BeatmapObjectSpawnController>().FirstOrDefault<BeatmapObjectSpawnController>();
-                if (spawnController) {
+                if (spawnController)
+                {
                     spawnController.noteDidStartJumpEvent -= injectNotes;
                     spawnController.noteDidStartJumpEvent += injectNotes;
                 }
+                if (colorManager == null)
+                    colorManager = Resources.FindObjectsOfTypeAll<ColorManager>().First();
             }
         }
 
-        public void OnSceneUnloaded(Scene scene) {
+        public void OnSceneUnloaded(Scene scene)
+        {
 
         }
 
-        private void injectNotes(BeatmapObjectSpawnController spawnContoller, NoteController noteContoller) {
-            try {
-                foreach (Transform child in noteContoller.gameObject.transform) {
-                    if (!child.Find("fakeMesh") && selectedNote != "DefaultNotes" && cubeMesh != null) {
-                        GameObject whichMesh;
-                        if (noteContoller.noteData.noteType == NoteType.NoteA) {
-                            whichMesh = cubeMeshRed;
-                        } else if (noteContoller.noteData.noteType == NoteType.NoteB) {
-                            whichMesh = cubeMesh;
-                        } else return;
-                        noteContoller.gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
-                        GameObject fakeMesh = new GameObject("fakeMesh");
-                        fakeMesh.transform.SetParent(child);
-                        MeshRenderer meshRender = fakeMesh.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
-                        MeshFilter meshFilter = fakeMesh.AddComponent(typeof(MeshFilter)) as MeshFilter;
-                        fakeMesh.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
-                        fakeMesh.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
-                        fakeMesh.transform.Rotate(new Vector3(-90, 0, 0), Space.Self);
-                        meshFilter.mesh = whichMesh.GetComponent<MeshFilter>().mesh;
-                        meshRender.material = whichMesh.GetComponent<Renderer>().material;
-                        Console.WriteLine(meshRender.material.name);
-                        Console.WriteLine(meshRender.material.shader.name);
+        private void injectNotes(BeatmapObjectSpawnController spawnContoller, NoteController noteContoller)
+        {
+            try
+            {
+                Transform child = noteContoller.gameObject.transform.Find("NoteCube");
+                GameObject.Destroy(child.Find("fakeMesh")?.gameObject);
+                if (selectedNote != "DefaultNotes" && noteLeft != null)
+                {
+                    GameObject customNote;
+                    if (noteContoller.noteData.noteType == NoteType.NoteA)
+                    {
+                        if (noteRight != null)
+                            customNote = noteRight;
+                        else
+                            customNote = noteLeft;
                     }
+                    else if (noteContoller.noteData.noteType == NoteType.NoteB)
+                    {
+                        customNote = noteLeft;
+                    }
+                    else return;
+                    var noteMesh = noteContoller.gameObject.GetComponentInChildren<MeshRenderer>();
+                    noteMesh.enabled = false;
+                    GameObject fakeMesh = GameObject.Instantiate(customNote);
+                    fakeMesh.transform.SetParent(child);
+                    fakeMesh.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+                    fakeMesh.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
+                    fakeMesh.transform.Rotate(new Vector3(-90, 0, 0), Space.Self);
+
                 }
 
-                if (selectedNote != "DefaultNotes") {
-                    if (cubeMesh != null) {
+
+                if (selectedNote != "DefaultNotes")
+                {
+                    if (noteLeft != null)
+                    {
                     }
                 }
-            } catch (Exception err) {
-                Console.WriteLine(err);
+            }
+            catch (Exception err)
+            {
+                Logger.log.Error(err);
             }
         }
 
-        public void LoadNoteAsset(string path) {
-            Console.WriteLine("LOADING " + path);
-            if (assetBundle != null) {
+        public void LoadNoteAsset(string path)
+        {
+         //   Console.WriteLine("LOADING " + path);
+            if (assetBundle != null)
+            {
                 assetBundle.Unload(true);
             }
 
-            if (path != "DefaultNotes") {
-                Console.WriteLine("STARTING");
+            if (path != "DefaultNotes")
+            {
+                noteLeft = null;
+                noteRight = null;
+          //      Console.WriteLine("STARTING");
                 selectedNote = path;
-                var isPath = IsValidPath(selectedNote);
-                Console.WriteLine("IS PATH: " + isPath);
-                if (isPath == true) {
+                assetBundle = AssetBundle.LoadFromFile(selectedNote);
+           //     var isPath = IsValidPath(selectedNote);
+        //        Console.WriteLine("IS PATH: " + isPath);
+        /*
+                if (isPath == true)
+                {
                     assetBundle = AssetBundle.LoadFromFile(selectedNote);
-                } else if (isPath == false) {
-                    try {
+                }
+                else if (isPath == false)
+                {
+                    try
+                    {
                         assetBundle = AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream(selectedNote));
-                    } catch {
-                        Console.WriteLine("Failed to load as resource stream - path does not exist");
+                    }
+                    catch
+                    {
+                        Logger.log.Warn("Failed to load as resource stream - path does not exist");
                     }
                 }
-                foreach (string assetName in assetBundle.GetAllAssetNames()) {
+                */
+                foreach (string assetName in assetBundle.GetAllAssetNames())
+                {
                     Console.WriteLine(assetName);
                 }
-                if (assetBundle == null) {
-                    Console.WriteLine("something went wrong getting the asset bundle!");
-                } else {
-                    Console.WriteLine("Succesfully obtained the asset bundle!");
-                    cubeMesh = assetBundle.LoadAsset<GameObject>("assets/blue_block.prefab");
-                    cubeMeshRed = assetBundle.LoadAsset<GameObject>("assets/red_block.prefab");
+                if (assetBundle == null)
+                {
+                    Logger.log.Warn("something went wrong getting the asset bundle!");
                 }
-            } else if (path == "DefaultNotes") {
-                if (assetBundle != null) {
+                else
+                {
+                   
+                    GameObject note = assetBundle.LoadAsset<GameObject>("assets/_customnote.prefab");
+                    selectedNoteDescriptor = note?.GetComponent<NoteDescriptor>();
+                    selectedNote = selectedNoteDescriptor?.NoteName;
+                    //         Console.WriteLine("Succesfully obtained the asset bundle!");
+                    //            foreach (var c in assetBundle.AllAssetNames())
+                    //                 Logger.log.Info(c);
+                   noteLeft = note.transform.Find("NoteLeft").gameObject;
+                    noteRight = note.transform.Find("NoteRight").gameObject;
+             //       noteLeft = assetBundle.LoadAsset<GameObject>("assets/blue_block.prefab");
+             //       if (noteLeft == null)
+             //           noteLeft = assetBundle.LoadAsset<GameObject>("assets/materials/cubemesh.prefab");
+
+//                    noteRight = assetBundle.LoadAsset<GameObject>("assets/red_block.prefab");
+                }
+            }
+            else
+            {
+                if (assetBundle != null)
+                {
                     assetBundle.Unload(true);
                     assetBundle = null;
-                    cubeMesh = null;
-                    cubeMeshRed = null;
+                    noteLeft = null;
+                    noteRight = null;
                 }
                 selectedNote = "DefaultNotes";
             }
-            Console.WriteLine($"Loaded custom note {path}");
+            Logger.log.Info($"Loaded custom note {selectedNote}");
         }
 
 
